dc27ea1345640d98c3c7f80c7bbfa3488e77c870
Andy.chan <74639510+AndyChanPundix@users.noreply.github.com>
2020-12-09 16:03:06 +0800

    Update util.swift

--- Application/Extension/util.swift
+++ Application/Extension/util.swift
@@ -1,19 +1,6 @@
-//
-//  util.swift
-//  XWallet
-//
-//  Created by 梅杰 on 2018/8/23.
-//  Copyright © 2018年 Chen Andy. All rights reserved.
-//
-
 import UIKit
 import WKKit
 
-/**
- * 千分位 和 小数点后有效位
- * @parameter string  传入字符串
- * @parameter precision 保留几位小数
- */
 func separatedFloatStrWith(_ string: String, afterPoint: Int) -> String {
 
     let formatter = NumberFormatter()
@@ -88,94 +75,48 @@ func separatedNo3FloatStrWith(_ string: String, afterPoint: Int) -> String {
     }
 }
 
-extension String {
-    // MARK: 添加千分位的函数实现
-    func addMicrometerLevel() -> String {
-        // 判断传入参数是否有值
-        if self.count != 0 {
-            /**
-             创建两个变量
-             integerPart : 传入参数的整数部分
-             decimalPart : 传入参数的小数部分
-             */
+extension String { 
+    func addMicrometerLevel() -> String { 
+        if self.count != 0 { 
             var integerPart: String?
             var decimalPart = String.init()
-
-            // 先将传入的参数整体赋值给整数部分
-            integerPart =  self
-            // 然后再判断是否含有小数点(分割出整数和小数部分)
+ 
+            integerPart =  self 
             if self.contains(".") {
                 let segmentationArray = self.components(separatedBy: ".")
                 integerPart = segmentationArray.first
                 decimalPart = segmentationArray.last!
-            }
-
-            /**
-             创建临时存放余数的可变数组
-             */
-            let remainderMutableArray = NSMutableArray.init(capacity: 0)
-            // 创建一个临时存储商的变量
-            var discussValue: Int32 = 0
-
-            /**
-             对传入参数的整数部分进行千分拆分
-             */
+            } 
+            let remainderMutableArray = NSMutableArray.init(capacity: 0) 
+            var discussValue: Int32 = 0 
             repeat {
-                let tempValue = integerPart! as NSString
-                // 获取商
-                discussValue = tempValue.intValue / 1000
-                // 获取余数
-                let remainderValue = tempValue.intValue % 1000
-                // 将余数一字符串的形式添加到可变数组里面
+                let tempValue = integerPart! as NSString 
+                discussValue = tempValue.intValue / 1000 
+                let remainderValue = tempValue.intValue % 1000 
                 let remainderStr = String.init(format: "%d", remainderValue)
-                remainderMutableArray.insert(remainderStr, at: 0)
-                // 将商重新复制
+                remainderMutableArray.insert(remainderStr, at: 0) 
                 integerPart = String.init(format: "%d", discussValue)
             } while discussValue>0
-
-            // 创建一个临时存储余数数组里的对象拼接起来的对象
+ 
             var tempString = String.init()
-
-            // 根据传入参数的小数部分是否存在，是拼接“.” 还是不拼接""
-            let lastKey = (decimalPart.count == 0 ? "":".")
-            /**
-             获取余数组里的余数
-             */
-            for i in 0..<remainderMutableArray.count {
-                // 判断余数数组是否遍历到最后一位
+ 
+            let lastKey = (decimalPart.count == 0 ? "":".") 
+            for i in 0..<remainderMutableArray.count { 
                 let  param = (i != remainderMutableArray.count-1 ?",":lastKey)
                 tempString = tempString + String.init(format: "%@%@", remainderMutableArray[i] as! String, param)
-            }
-            //  清楚一些数据
+            } 
             integerPart = nil
-            remainderMutableArray.removeAllObjects()
-            // 最后返回整数和小数的合并
+            remainderMutableArray.removeAllObjects() 
             return tempString as String + decimalPart
         }
         return self
     }
-
-    // MARK: 获取字符串的长度
-    func length() -> Int {
-        /**
-         另一种方法：
-         let tempStr = self as NSString
-         return tempStr.length
-         */
+ 
+    func length() -> Int { 
         return self.count
     }
 }
-
-// 数字格式化............
-//func separatedNoFloatStrWith(_ b: Double, afterPoint: Int) -> String {
-//    return  separatedFloatStrWith( "\(b)" as NSString, precision: afterPoint)
-//}
-//
-//
-//func not_Rounding(_ b: Double, afterPoint: Int) -> String {
-//    return separatedNoFloatStrWith(string: "\(b)" as NSString, precision: afterPoint)
-//}
-
+ 
 extension String {
     func timeString() -> String {
         if self.length == 0 {
@@ -187,8 +128,7 @@ extension String {
             }
             let timeStamp = TimeInterval(Int(time/1000))
             let date = Date(timeIntervalSince1970: timeStamp)
-            let formatter = DateFormatter()
-            //          formatter.dateFormat = "YYYY-MM-dd HH:mm:ss zzz"
+            let formatter = DateFormatter() 
             formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
             return formatter.string(from: date)
         }
@@ -229,13 +169,11 @@ extension Double {
         }
         return formatter.string(from: date)
     }
-
-    ///四舍五入 到小数点后某一位
+ 
     func roundTo(places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
-    }
-    ///截断  到小数点后某一位
+    } 
     func truncate(places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return Double(Int(self * divisor)) / divisor
@@ -246,23 +184,14 @@ func isDeLanguage() -> Bool {
     return WKLocale.Shared.localeIdentifier == "de"
 }
 
-extension String {
-    //字符串截取
+extension String { 
     func separatedEightString() -> String {
         if let value = self.firstIndex(of: "."), let deRange = self.range(of: ".") {
             let backNumber = self.suffix(from: deRange.upperBound)
             if  String(backNumber).length > 8 {
                 let index2 = self.index(value, offsetBy: 8)
                 return String(self[self.startIndex...index2])
-            }
-            //            //截取小数点前字符(不包含小数点)  123
-            //            let wholeNumber = num.prefix(upTo: deRange!.lowerBound)
-            //            //截取小数点后字符(不包含小数点) 45
-            //            let backNumber = num.suffix(from: deRange!.upperBound)
-            //            //截取小数点前字符(包含小数点) 123.
-            //            let wholeNumbers = num.prefix(upTo: deRange!.upperBound)
-            //            //截取小数点后字符(包含小数点) .45
-            //            let backNumbers = num.suffix(from: deRange!.lowerBound)
+            } 
         }
         return self
     }
@@ -294,8 +223,7 @@ extension Date {
     var timestamp: Double {
         return self.timeIntervalSince1970 * 1000
     }
-
-    /// 获取当前 毫秒级 时间戳 - 13位
+ 
     public var milliStamp: Int64 {
         let timeInterval: TimeInterval = self.timeIntervalSince1970
         let time = CLongLong(round(timeInterval*1000))
